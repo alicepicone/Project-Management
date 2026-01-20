c845a5d44d0c741fe198c04cdc6a0ca146c6d050
alicepicone <alice.picone17@gmail.com>
2026-01-20 15:17:45 +0100

    create task for insert new project and save it

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,41 @@ function App() {
     projects: []
   });
 
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectProjectId: null
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject =  {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectsState.selectProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="n-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }

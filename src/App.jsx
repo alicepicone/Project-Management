9a719f2f632f216b31e46e0d5f13856c8722fd5c
alicepicone <alice.picone17@gmail.com>
2026-01-20 18:12:33 +0100

    add possibility to cancel tasks

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,13 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {
-    
+  function handleDeleteTask(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id)  
+      };
+    });
   }
 
   function handleSelectProject(id) {
@@ -113,6 +118,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects} 
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectProjectId}
       />
       {content}
     </main>
